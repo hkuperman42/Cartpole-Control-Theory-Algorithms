5750f0c8213b3a5aae1e281199eb563a3d9f83aa
hkuperman3 <kup@gatech.edu>
2023-02-11 21:24:43 -0500

    Folderfy

---
+++ utilities/getCircleMask.m
@@ -0,0 +1,21 @@
+function [mask] = getCircleMask(xDim, yDim, xCenter, yCenter, radius)
+%getCircleMask Generates a mask of a circle
+
+%Preallocate space for the mask
+mask = zeros(yDim, xDim, 'logical');
+
+%Define the angles we need to cover
+theta = linspace(0, 2*pi, 4 * pi * radius);
+
+%Get x and y vectors for each point along the circumference.
+x = radius * cos(theta) + xCenter;
+y = radius * sin(theta) + yCenter;
+
+%Assemble those coordinates into a mask
+for k = 1 : length(x)
+    row = round(y(k));
+    col = round(x(k));
+    if col > 0 & col < 1600
+        mask(row, col) = 1;
+    end
+end
---
+++ utilities/splitFive.m
@@ -0,0 +1,10 @@
+function [i1, i2, i3, i4, i5] = splitFive(i)
+%splitFive Splits a vector into its five component parts. There's probably
+%a better way to do this
+i1 = i(1);
+i2 = i(2);
+i3 = i(3);
+i4 = i(4);
+i5 = i(5);
+end
+
